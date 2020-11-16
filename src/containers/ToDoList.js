a06a4a2abd7f1a7429d30160883b9c7efebb1fd8
Bret Merritt <merrittbret9@gmail.com>
2020-11-16 10:54:12 -0700

    Add TodoList with mapStateToProps and connect

---
+++ src/containers/ToDoList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { connect } from 'react-redux';
+const ToDoList = ({todos}) => {
+  console.log(todos);
+  return (
+    <ul>
+    </ul>
+  )
+}
+
+const mapStateToProps = state => ({
+  todos: state.todos
+});
+
+export default connect(mapStateToProps)(ToDoList);

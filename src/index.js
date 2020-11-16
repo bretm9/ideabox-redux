1e5b29b2270b0a82b8db5942fe4253eefe45ae2d
Bret Merritt <merrittbret9@gmail.com>
2020-11-16 10:03:42 -0700

    Fix composeWithDevTools import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import composeWithDevTools from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
 import App from './components/App';
 import './index.css';

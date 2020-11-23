cf96fa575c1aa44ddcf282c26d33c7dfd0952cff
Bret Merritt <merrittbret9@gmail.com>
2020-11-23 13:37:42 -0700

    Updates

---
+++ precache-manifest.2d42d6e2a619054a1b534f98d4de0cdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e48f89ec9fa762c2b5f7c58d04ebca71",
+    "url": "/ideabox-redux/index.html"
+  },
+  {
+    "revision": "bfff22dccd6d2308b343",
+    "url": "/ideabox-redux/static/css/main.fc956907.chunk.css"
+  },
+  {
+    "revision": "8faeec026a65e7e6df31",
+    "url": "/ideabox-redux/static/js/2.7fd549de.chunk.js"
+  },
+  {
+    "revision": "6c46089e713fdf01ec7e89132bc85450",
+    "url": "/ideabox-redux/static/js/2.7fd549de.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bfff22dccd6d2308b343",
+    "url": "/ideabox-redux/static/js/main.1c71c52b.chunk.js"
+  },
+  {
+    "revision": "f0d8499faa42cb1c17b5",
+    "url": "/ideabox-redux/static/js/runtime-main.67703f24.js"
+  }
+]);
